5a3ae8ffceb4f7df70e31a070055f61749338aac
joshyahweh <joshojoawo@gmail.com>
2022-06-09 00:30:13 +0100

    Snap | Remote Work

---
+++ script.js
@@ -0,0 +1,14 @@
+// "use strict";
+// const nav = document.querySelector(".nav");
+// const handleHover = (e) => {
+//   const link = e.target;
+//   if (link.classList.contains("nav__link-left")) {
+//     const siblings = link.closest(".nav").querySelectorAll(".nav__link-left");
+
+//     siblings.forEach((el) => {
+//       if (el !== link) el.style.opacity = opacity;
+//     });
+//   }
+// };
+// nav.addEventListener("mouseover", handleHover.bind(0.5));
+// nav.addEventListener("mouseout", handleHover.bind(0.5));
